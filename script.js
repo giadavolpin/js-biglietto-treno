ea67ee5d123ae7bbb2b81e723000f10537043970
giadavolpin <volpingiada@gmail.com>
2022-10-06 17:49:46 +0200

    minorenni

--- script.js
+++ script.js
@@ -7,15 +7,26 @@ secondo queste regole:
 il prezzo del biglietto è definito in base ai km (0.21 € al km)
 va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
-L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). 
+L'output del prezzo finale va messo fuori in forma umana 
+(con massimo due decimali, per indicare centesimi sul prezzo). 
 Questo richiederà un minimo di ricerca. */
 
 let kmUtente = prompt ('inserisci km che vuoi fare');
 let etaUtente = prompt('inserisci la tua eta');
+const minorenni = (<18 - 20%);
+const maggiorenni =(>65 - 40%); 
 
-let ticketCost = prompt (' ')
+if(minorenni){
+    
+}
+if (maggiorenni) {
+    
+}
 
-console.log  (ticketCost);
+
+ticketCost = 0.21 * kmUtente ; 
+
+console.log (ticketCost);
 
 
 
