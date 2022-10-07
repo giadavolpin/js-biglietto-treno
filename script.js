26422ba71a08427866fbc63d4d9d52b752ada54c
giadavolpin <volpingiada@gmail.com>
2022-10-07 10:25:16 +0200

    sconto

--- script.js
+++ script.js
@@ -9,24 +9,47 @@ va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
 L'output del prezzo finale va messo fuori in forma umana 
 (con massimo due decimali, per indicare centesimi sul prezzo). 
-Questo richiederà un minimo di ricerca. */
+Questo richiederà un minimo di ricerca.
 
-let kmUtente = prompt ('inserisci km che vuoi fare');
-let etaUtente = prompt('inserisci la tua eta');
-const minorenni = (<18 - 20%);
-const maggiorenni =(>65 - 40%); 
+Chiedo all'utente numero km 
+chiedo all'utente la sua età
+prezzo al km 0.21
+calcolare prezzo biglietto (numeroKM * prezzoKm)
+sconto under 18 0.20
+sconto over 65 0.40
+se utente età under 18 prezzo biglietto 
+    prezzoBiglietto * sconto Under18 
+Altrimenti se utente età over65 prezzo biglietto prezzoBiglietto
+    prezzoBiglietto * scontoOver65
+*/
 
-if(minorenni){
-    
+const numeroKm = parseInt(prompt ('Quanti chilometri vuoi percorrere?'))
+const etaUtente = parseInt(prompt('Quanti anni hai?'))
+
+if(isNaN (numeroKm) || isNaN(etaUtente)) {
+    alert('devi inserire numeri');
+
 }
-if (maggiorenni) {
-    
+
+/* console.log(numeroKm, etaUtente); */
+const prezzoKm = 0.21
+const scontoUnder18 = 0.20
+const scontoOver65 = 0.40 
+let prezzoBiglietto = numeroKm * prezzoKm
+
+let messaggio = `
+<h1> Il costo del tuo biglietto è:${prezzoBiglietto} €</h1> `
+/* console.log (prezzoBiglietto) */
+
+
+
+if(etaUtente <= 18){
+    prezzoBiglietto = prezzoBiglietto - (prezzoBiglietto * scontoUnder18)
+    messaggio += ${prezzoBiglietto} €  `Hai ricevuto uno sconto del 20%`;
+
+} else if (etaUtente >= 65){
+    prezzoBiglietto = prezzoBiglietto - (prezzoBiglietto + scontoOver65)
+    messaggio += ${prezzoBiglietto} € `Hai ricevuto uno sconto del 40%`;
 }
-
-
-ticketCost = 0.21 * kmUtente ; 
-
-console.log (ticketCost);
-
 
 
